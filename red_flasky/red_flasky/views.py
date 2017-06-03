247b3bc771eb7dce45574b22327895d781ea1c9b
Zaighum Rajput <zaighum47@gmail.com>
2017-06-02 23:21:52 -0400

    input form working

--- red_flasky/red_flasky/views.py
+++ red_flasky/red_flasky/views.py
@@ -3,16 +3,18 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request
 from red_flasky import app
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     """Renders the home page."""
+    input = request.form.get("publication") * 2
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        result=input
     )
 
