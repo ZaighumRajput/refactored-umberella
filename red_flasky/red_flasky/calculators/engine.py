d4493c37c2de21eb78cd95b33026420642f65f58
Zaighum Rajput <zaighum47@gmail.com>
2017-06-07 07:32:01 -0400

    list working

---
+++ red_flasky/red_flasky/calculators/engine.py
@@ -0,0 +1,8 @@
+class engine(object):
+    """description of class"""
+    def __init__(self):
+        self.todolist = []
+
+    def addTask(self, task):
+        self.todolist.append(task)
+
--- red_flasky/red_flasky/views.py
+++ red_flasky/red_flasky/views.py
@@ -2,19 +2,21 @@
 Routes and views for the flask application.
 """
 
-from datetime import datetime
-from flask import render_template, request
-from red_flasky import app
+from datetime     import datetime
+from flask        import render_template, request
+from red_flasky   import app
+from .calculators import engine
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     """Renders the home page."""
-    input = request.form.get("publication") * 2
+    todoList = engine.engine()
+    todoList.addTask(request.form.get("publication"))
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
-        result=input
+        result=todoList.todolist
     )
 
