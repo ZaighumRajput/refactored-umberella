8adc7b33ddc7e15b021c025b1e39e9bdf5fe6d66
Zaighum Rajput <zaighum47@gmail.com>
2017-06-07 08:26:39 -0400

    basic expense list dictionary = iteritems

--- red_flasky/red_flasky/calculators/engine.py
+++ red_flasky/red_flasky/calculators/engine.py
@@ -6,4 +6,12 @@ class engine(object):
     def addTask(self, task):
         self.todolist.append(task)
 
+class budget(object):
+
+    def __init__(self):
+        self.income = 0
+        self.expenses ={'weed': 10.00}
+
+    def addExpense(self, item, cost):
+        self.expenses[item] = cost
 
--- red_flasky/red_flasky/views.py
+++ red_flasky/red_flasky/views.py
@@ -8,16 +8,17 @@ from red_flasky   import app
 from .calculators import engine
 
 todoList = engine.engine()
+budget   = engine.budget()
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     """Renders the home page."""
-    todoList.addTask(request.form.get("publication"))
+    budget.addExpense(request.form.get("item"), request.form.get("cost"))
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
-        result=todoList.todolist
+        expenses=budget.expenses
     )
 
