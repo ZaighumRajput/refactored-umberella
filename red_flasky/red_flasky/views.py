d458d26ef6130cb94d9fb6cddf9c272ec72d0871
Zaighum Rajput <zaighum47@gmail.com>
2017-06-24 09:09:32 -0400

    basic javascript examle

--- red_flasky/red_flasky/views.py
+++ red_flasky/red_flasky/views.py
@@ -7,18 +7,16 @@ from flask        import render_template, request
 from red_flasky   import app
 from .calculators import engine
 
-todoList = engine.engine()
-budget   = engine.budget()
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
     """Renders the home page."""
-    budget.addExpense(request.form.get("item"), request.form.get("cost"))
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-        expenses=budget.expenses
+        title   ='Home Page',
+        year    =datetime.now().year,
+        player1 = "Bunty",
+        player2 = "Za"
     )
 
